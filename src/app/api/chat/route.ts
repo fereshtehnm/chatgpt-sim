2e5e28332e2632c1f22cb9ae3d2e7e1596537f1d
Fereshteh <fereshte.namazi.edu@gmail.com>
2024-08-18 17:51:10 +0330

    Build - button component in ui

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,23 +1,48 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { NextResponse } from 'next/server'
+import { clerkClient, currentUser } from '@clerk/nextjs'
 
-// Create an OpenAI API client
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+export const runtime = 'edge'
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || '' })
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  try {
+    if (!process.env.OPENAI_API_KEY) {
+      return new NextResponse('Missing OpenAI API Key.', { status: 400 })
+    }
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
-  });
+    const user = await currentUser()
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    if (!user) {
+      return new NextResponse('You need to sign in first.', { status: 401 })
+    }
+
+    const credits = Number(user.publicMetadata?.credits || 0)
+
+    if (!credits) {
+      return new NextResponse('You have no credits left.', { status: 402 })
+    }
+
+    const { messages } = await req.json()
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages
+    })
+
+    // Deduct credits
+    await clerkClient.users.updateUserMetadata(user.id, {
+      publicMetadata: {
+        credits: credits - 1
+      }
+    })
+
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream)
+  } catch (error: any) {
+    return new NextResponse(error.message || 'Something went wrong!', {
+      status: 500
+    })
+  }
 }
